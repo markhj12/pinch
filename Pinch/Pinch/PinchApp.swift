fa42fb8984c8f95e81fd85a4f2856b9a8f10b9b0
markhew <markhewjin@gmail.com>
2023-01-25 10:02:54 +0800

    initial commit

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mark Hew on 25/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
