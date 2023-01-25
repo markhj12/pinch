3cfd4f18b15566cfc042ef78908560e347cb9502
markhew <markhewjin@gmail.com>
2023-01-25 13:16:27 +0800

    drag

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -11,6 +11,14 @@ struct ContentView: View {
     //MARK: PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    func resetImage() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         VStack {
@@ -24,6 +32,7 @@ struct ContentView: View {
                         .padding()
                         .shadow(color: .black.opacity(0.2), radius: 12, x:2, y:2)
                         .opacity(isAnimating ? 1 : 0)
+                        .offset(x:imageOffset.width, y:imageOffset.height)
                         .scaleEffect(imageScale)
                     //MARK: DOUBLE TAP
                         .onTapGesture(count: 2, perform: {
@@ -32,12 +41,25 @@ struct ContentView: View {
                                     imageScale = 5
                                 }
                             } else {
-                                withAnimation(.spring()){
-                                    imageScale = 1
-                                }
+                                resetImage()
                             }
                                 
                         })
+                        //MARK: DRAG
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ value in
+                                    withAnimation(.linear(duration:1)){
+                                        imageOffset = value.translation
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if(imageScale <= 1){
+                                        resetImage()
+                                    }
+                                })
+                        )
+                    
                 }//: ZStack
                 .navigationTitle("Pinch & Zoom")
                 .navigationBarTitleDisplayMode(.inline)
@@ -53,6 +75,8 @@ struct ContentView: View {
         .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
